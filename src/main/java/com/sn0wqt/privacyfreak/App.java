91df9885cfc3446cb60943f41f39ee7cd0aa2d0a
sn0wqt <asadfalah2@gmail.com>
2025-04-19 13:52:03 +0300

    project init

---
+++ src/main/java/com/sn0wqt/privacyfreak/App.java
@@ -0,0 +1,11 @@
+package com.sn0wqt.privacyfreak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
